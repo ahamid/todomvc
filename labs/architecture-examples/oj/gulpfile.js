1141c7b55036895cc4afdb8e274547f963d3b355
Aaron Hamid <aaron.hamid@gmail.com>
2014-06-09 01:31:05 -0400

    archive backbone impl; load ojs deps in index.html; use lodash only, not underscore

--- labs/architecture-examples/oj/gulpfile.js
+++ labs/architecture-examples/oj/gulpfile.js
@@ -3,14 +3,26 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 var gulpBowerFiles = require('gulp-bower-files');
 var print = require('gulp-print');
+var colors = require('colors');
+var path = require('path');
 
 gulp.task('bower-install', function() {
   return bower();
 });
 
 gulp.task('bower', ['bower-install'], function() {
-  return gulpBowerFiles().pipe(print())
-                         .pipe(gulp.dest('bower_components'));
+  return gulpBowerFiles()
+           .pipe(gulp.dest('bower_components'))
+           .pipe(print(function (filepath) {
+             var plain_filepath = colors.stripColors(filepath);
+             if (/.*\.js$/i.test(plain_filepath)) {
+               return '<script src="' + plain_filepath + '"></script>';
+             } else {
+               return '';
+             }
+           }));
 });
 
 gulp.task('default', ['bower']);
+
+if (/.*\.js$/i.test("foo.js")) console.log("YES");
