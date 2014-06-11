59dab5bd21d616b0d74e0f8bd4eaf8b90c13ef41
Aaron Hamid <aaron.hamid@gmail.com>
2014-06-11 15:07:12 -0400

    updated todos storage and added test

--- labs/architecture-examples/oj/js/models/todos.js
+++ labs/architecture-examples/oj/js/models/todos.js
@@ -3,40 +3,64 @@
 
 var app = app || {};
 
-$(function () {
-  'use strict';
+'use strict';
 
+(function() {
   var Todos = app.Todos = function (name) {
     this.name = name || 'todomvc-oj';
-    this.counter = -1;
     var store = localStorage[this.name];
-    this.index = (store && store.split(',')) || [];
-  }
-
-  Todos.nextId = function() {
-    counter++;
-    return counter;
+    loadIndex.call(this, (store && store.split(',')) || []);
   };
 
-  Todos.add = function(obj) {
-    var key = this.name + '-' + this.nextId();
+  function parseItemIndex(key) {
+    return
+  }
+
+  function loadIndex(index) {
+    this.index = index;
+    this.counter = this.index.length > 0 ? this.index[0].split('-').pop() : -1;
+  }
+
+  function saveIndex() {
+    localStorage[this.name] = this.index.join(',');
+  }
+
+  function nextId() {
+    this.counter++;
+    return this.counter;
+  }
+
+  Todos.prototype.add = function(obj) {
+    var key = this.name + '-' + nextId.call(this);
     this.index.unshift(key);
     localStorage[key] = JSON.stringify(obj);
-    localStorage[this.name] = JSON.stringify(this.index);
-  }
+    saveIndex.call(this);
+  };
 
-  Todos.deleteAt = function(idx) {
+  Todos.prototype.deleteAt = function(idx) {
     var key = this.index.splice(idx, 1);
-    localStorage[this.name] = JSON.stringify(this.index);
+    saveIndex.call(this);
     localStorage.removeItem(key);
-  }
+  };
 
-  Todos.itemAt = function(idx) {
+  Todos.prototype.itemAt = function(idx) {
     return JSON.parse(localStorage.getItem(this.index[idx]));
-  }
+  };
 
-  Todos.updateAt = function(idx, obj) {
+  Todos.prototype.updateAt = function(idx, obj) {
     localStorage[this.index[idx]] = JSON.stringify(obj);
-  }
+  };
 
-});
+  Todos.prototype.size = function () {
+    return this.index.length;
+  };
+
+  Todos.prototype.clear = function() {
+    var prevIndex = this.index;
+    this.index = [];
+    prevIndex.forEach(function(key) {
+      localStorage.removeItem(key);
+    });
+    saveIndex.call(this);
+  };
+})();
--- labs/architecture-examples/oj/test/todos.js
+++ labs/architecture-examples/oj/test/todos.js
@@ -1,8 +1,174 @@
 describe('Todos', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+  var todos = new app.Todos();
+
+  var an_item = { testAdd: "testAdd" };
+  var another_item = { testAdd2: "testAdd2" };
+  function clear() {
+    after(function() {
+      todos.clear();
     })
-  })
-})
+  }
+
+  describe('initially', function() {
+    it('should be empty', function() {
+      assert.equal(0, todos.size());
+    })
+  });
+
+  function populateItems() {
+    before(function() {
+      todos.add(an_item);
+      todos.add(another_item);
+    });
+  }
+
+  function behavesAsInitiallyPopulated() {
+    it('should should have 2 items', function(){
+      assert.equal(2, todos.size());
+    });
+    it('should should prepend the second added item', function() {
+      assert.deepEqual({testAdd2: "testAdd2"}, todos.itemAt(0));
+    });
+    it('should should contain the first added item', function() {
+      assert.deepEqual({testAdd: "testAdd"}, todos.itemAt(1));
+    });
+  }
+
+  describe('#add()', function(){
+    before(function() {
+      todos.add(an_item);
+    });
+
+    describe("first item", function() {
+      it('should should have 1 item', function(){
+        assert.equal(1, todos.size());
+      });
+      it('should should allow access to the first added item by index', function() {
+        assert.deepEqual({testAdd: "testAdd"}, todos.itemAt(0));
+      });
+    });
+
+    describe("second item", function() {
+      before(function() {
+        todos.add(another_item);
+      });
+
+      behavesAsInitiallyPopulated();
+    });
+
+    clear();
+  });
+
+  describe('#updateAt()', function(){
+    populateItems();
+
+    describe("before update", behavesAsInitiallyPopulated);
+
+    describe("after update", function() {
+      before(function() {
+        todos.updateAt(1, { testAddUpdated: "testAddUpdated" });
+        todos.updateAt(0, { testAddUpdated2: "testAddUpdated2" });
+      });
+      it('should should have 2 items', function(){
+        assert.equal(2, todos.size());
+      });
+      it('should should update the first added item', function() {
+        assert.deepEqual({testAddUpdated: "testAddUpdated"}, todos.itemAt(1));
+      });
+      it('should should update the second added item', function() {
+        assert.deepEqual({testAddUpdated2: "testAddUpdated2"}, todos.itemAt(0));
+      });
+    });
+
+    clear();
+  });
+
+  describe('#deleteAt()', function(){
+    describe('delete first, then second', function() {
+
+      populateItems();
+
+      describe("before delete", behavesAsInitiallyPopulated);
+
+      describe("after delete of first added item", function() {
+        before(function() {
+          todos.deleteAt(1);
+        });
+
+        it('should should have 1 items', function(){
+          assert.equal(1, todos.size());
+        });
+
+        it('should should contain the second added item', function() {
+          assert.deepEqual({testAdd2: "testAdd2"}, todos.itemAt(0));
+        });
+      });
+
+
+      describe("after delete of second added item", function() {
+        before(function() {
+          todos.deleteAt(0);
+        });
+
+        it('should should be empty', function(){
+          assert.equal(0, todos.size());
+        });
+      });
+
+      clear();
+    });
+
+    describe('delete second, then first', function() {
+
+      populateItems();
+
+      describe("before delete", behavesAsInitiallyPopulated);
+
+
+      describe("after delete of second added item", function() {
+        before(function() {
+          todos.deleteAt(0);
+        });
+
+        it('should should have 1 items', function(){
+          assert.equal(1, todos.size());
+        });
+
+        it('should should contain the first added item', function() {
+          assert.deepEqual({testAdd: "testAdd"}, todos.itemAt(0));
+        });
+      });
+
+      describe("after delete of first added item", function() {
+        before(function() {
+          todos.deleteAt(0);
+        });
+
+        it('should should be empty', function(){
+          assert.equal(0, todos.size());
+        });
+      });
+
+      clear();
+    });
+  });
+
+  describe('should persist items', function() {
+    populateItems();
+
+    describe("recreated Todos object", function() {
+      before(function() {
+        todos = new app.Todos();
+      });
+
+      behavesAsInitiallyPopulated();
+
+      it("accepts new items", function() {
+        todos.add({itemThree: "itemThree"});
+        assert.deepEqual({itemThree: "itemThree"}, todos.itemAt(0));
+      });
+    });
+
+    clear();
+  });
+});
